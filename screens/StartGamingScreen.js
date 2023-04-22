2e93afbd6209de9e9b173ccd130f080444afc15a
erick <eriickdeveloper@gmail.com>
2023-04-22 13:03:07 -0300

    starting project

---
+++ screens/StartGamingScreen.js
@@ -0,0 +1,13 @@
+import { TextInput, View } from "react-native";
+
+const StartGamingScreen = () => {
+  return (
+    <View>
+      <TextInput />
+
+      
+    </View>
+  );
+};
+
+export default StartGamingScreen;
