fd60c24689405ace841bf742f861ff967f16f274
erick <eriickdeveloper@gmail.com>
2023-04-22 13:15:00 -0300

    starting create screens

--- screens/StartGamingScreen.js
+++ screens/StartGamingScreen.js
@@ -1,11 +1,14 @@
 import { TextInput, View } from "react-native";
 
+import PrimaryButton from "../components/PrimaryButton";
+
 const StartGamingScreen = () => {
   return (
     <View>
       <TextInput />
 
-      
+      <PrimaryButton>Resetar</PrimaryButton>
+      <PrimaryButton>Confimar</PrimaryButton>
     </View>
   );
 };
